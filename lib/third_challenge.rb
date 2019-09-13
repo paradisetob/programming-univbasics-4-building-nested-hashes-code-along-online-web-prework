0be293ceb8d557fbdf574d915b98444b975b01dd
tracey brimage <onelovenilynn@gmail.com>
2019-09-13 16:10:30 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,14 +1,14 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {:name=>"LordvMontague"
-        #your key/value pairs here
+      :patriarch => {
+        :name=>"Lord Montague",
+        :age=>"53"
         },
-      :matriarch => {
-        #your key/value pairs here
+      :matriarch => {:name=>"Lady Montague",
+        :age=>"54"
         },
-      :hero => {
-        #your key/value pairs here
+      :hero => {:name=>"Romeo",:age=>"15"
         },
       :hero_friends => []
    },
