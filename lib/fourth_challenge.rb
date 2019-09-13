1f58cc98fe82fa6854f7eba19912d9fc0a25ab02
tracey brimage <onelovenilynn@gmail.com>
2019-09-13 16:29:07 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,7 +5,7 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-      :Name=>"Benvolio" , :age=>"17", :attitude=>"worried",
+      :Name=>"Benvolio" , :age=>"17", :attitude=>"worried",:name=>"Mercutio",:age=>"18",:attitude=>"hot headed"
       ]
    },
    :capulet => {
