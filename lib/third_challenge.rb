f7b2e82db5d594a750ce0e02a1f44a6ea70c0c7f
tracey brimage <onelovenilynn@gmail.com>
2019-09-13 15:57:03 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,7 +1,7 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {
+      :patriarch => {:name=>"LordvMontague"
         #your key/value pairs here
         },
       :matriarch => {
@@ -11,7 +11,7 @@ def third_challenge
         #your key/value pairs here
         },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {
         #your key/value pairs here
@@ -26,5 +26,5 @@ def third_challenge
    }
   }
 
-  
+
 end
